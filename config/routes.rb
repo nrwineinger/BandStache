d83b415342094ca272bce52c053dcea937eea28f
nrwineinger <nrwineinger@gmail.com>
2015-12-29 22:28:46 -0700

    added glyphs, and changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ BandStache::Application.routes.draw do
 
   devise_for :users
   devise_for :installs
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
